7c03c5d272032848f25bb36cf506cec66ea3701e
Tilman Miraß <>
2020-07-10 16:49:02 +0200

    make draggable

--- src/pages/layout/quote.jsx
+++ src/pages/layout/quote.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 
 import AcaWhite from "../../components/svg/acaWhite";
 import ColouredBar from "../../components/svg/colouredBar";
--- src/pages/layout/quoteWithImage.jsx
+++ src/pages/layout/quoteWithImage.jsx
@@ -2,13 +2,17 @@ import React, { useEffect, useRef, useState } from "react";
 
 import AcaWhite from "../../components/svg/acaWhite";
 import ColouredBar from "../../components/svg/colouredBar";
+import Draggable from "react-draggable";
+import { Link } from "gatsby";
 import htmlToImage from "html-to-image";
 import slugify from "react-slugify";
 
 export default () => {
-  const [quoteText, setQuoteText] = useState(null);
-  const [position, setPosition] = useState(null);
-  const [name, setName] = useState(null);
+  const [image, setImage] = useState(null);
+  const [quoteText, setQuoteText] = useState("Hier kommt der Zitattext rein.");
+  const [name, setName] = useState("Name der Person");
+  const [position, setPosition] = useState("Position");
+  const [imagePosition, setImagePosition] = useState({ x: 0, y: 0 });
   const sharepicRef = useRef(null);
 
   const html2image = () => {
@@ -24,45 +28,98 @@ export default () => {
 
   return (
     <div className="container p-5">
+      <Link to="/">zurück zur Auswahl</Link>
       <div className="grid grid-cols-12 col-gap-2">
         <div className="col-span-12 sm:col-span-9 flex justify-center">
+          <br />
           <div
-            style={{ width: "600px", height: "600px" }}
-            className="grid grid-cols-12 col-gap-2 bg-orange relative"
+            style={{
+              width: "600px",
+              height: "600px",
+            }}
+            className="grid grid-cols-8 col-gap-2 relative"
             ref={sharepicRef}
           >
-            <div className="col-span-8 col-start-3 text-center flex items-end justify-center text-white">
+            <Draggable
+              onDrag={(e, data) => {
+                setImagePosition({ x: data.x, y: data.y });
+              }}
+            >
+              <div
+                className="absolute top-0 left-0 w-full h-full z-50"
+                draggable
+              ></div>
+            </Draggable>
+            {image !== null && (
+              <img
+                src={image}
+                className="absolute top-0 left-0 z-10"
+                style={{
+                  height: "100%",
+                  top: `${imagePosition.y}px`,
+                  left: `${imagePosition.x}px`,
+                }}
+              />
+            )}
+            <div
+              className="absolute top-0 left-0 w-full h-full z-20"
+              style={{
+                backgroundImage:
+                  "linear-gradient(180deg, #fff 0%, #000000 120%)",
+                mixBlendMode: "multiply",
+              }}
+            />
+
+            <div className="col-span-6 col-start-2 text-left flex items-center text-white z-30">
               <div>
                 <span
                   dangerouslySetInnerHTML={{ __html: quoteText }}
-                  className="block"
                   style={{ whiteSpace: "pre-line" }}
-                  className={`${(name !== "" || position !== "") && "mb-6"} block italic font-bold text-2xl leading-none`}
+                  className={`${
+                    (name !== "" || position !== "") && "mb-6"
+                  } block italic font-bold text-2xl leading-none`}
                 />
                 <div>
-                  <span dangerouslySetInnerHTML={{ __html: name }} />
+                  <span
+                    dangerouslySetInnerHTML={{ __html: name }}
+                    className="block font-bold text-md"
+                  />
                   <span
                     dangerouslySetInnerHTML={{ __html: position }}
-                    className={`${name !== "" && "ml-4"}`}
+                    style={{
+                      whiteSpace: "pre-line",
+                      display: position === "" ? "none" : "block",
+                    }}
                   />
                 </div>
               </div>
             </div>
             <div
-              className="col-span-12 flex justify-center"
+              className="col-span-12 flex justify-center absolute bottom-0 left-0 w-full"
               style={{ height: "4rem" }}
             >
-              <AcaWhite width="200" className="absolute bottom-0" />
+              <AcaWhite width="200" className="absolute bottom-0 z-30" />
             </div>
-            <ColouredBar width="600" className="absolute bottom-0" />
+            <ColouredBar width="600" className="absolute bottom-0 z-30" />
           </div>
         </div>
         <div className="col-span-12 sm:col-span-3">
+          <input
+            type="file"
+            onChange={(e) =>
+              e.target.files[0] !== null &&
+              setImage(URL.createObjectURL(e.target.files[0]))
+            }
+          />
+
           <label htmlFor="quoteText" className="block">
             Zitattext
           </label>
           <textarea
             id="quoteText"
+            rows={4}
+            cols={30}
+            defaultValue={quoteText}
             onChange={(e) => setQuoteText(e.target.value)}
             className="border-2 border-black"
           />
@@ -71,16 +128,20 @@ export default () => {
           </label>
           <input
             id="name"
+            defaultValue={name}
             onChange={(e) => setName(e.target.value)}
             className="border-2 border-black"
           />
           <label htmlFor="position" className="block">
             Position
           </label>
-          <input
+          <textarea
             id="position"
+            defaultValue={position}
             onChange={(e) => setPosition(e.target.value)}
             className="border-2 border-black"
+            rows={2}
+            cols={30}
           />
           <button
             className="block border-2 border-black p-1 mt-2"
